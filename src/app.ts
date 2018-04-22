3b6e7bcee0f615ec9949d01d82b1db57da202e5b
multios12 <multiple12@gmail.com>
2018-04-22 21:31:23 +0900

    feat: browser-sync・nodemonを追加

--- src/app.ts
+++ src/app.ts
@@ -13,17 +13,27 @@ app.set('view engine', 'ejs');
 
 app.use(compression());
 app.use(logger('dev'));
+
+// browser-sync Setup 
+if ( app.get('env') == 'development' ) {
+  var browserSync = require('browser-sync');
+  var connectBrowserSync = require('connect-browser-sync');
+
+  var browserSyncConfigurations = { "files": path.join(__dirname, "../views/*") };
+  app.use(connectBrowserSync(browserSync(browserSyncConfigurations)));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'sample')));
+app.use(express.static(path.join(__dirname, '../sample')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
 
 app.use(function (req, res, next) { next(createError(404)) });
-/*
-app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+
+app.use(function (err:any, req:express.Request, res: express.Response, next:Function) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -32,5 +42,5 @@ app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
   res.status(err.status || 500);
   res.render('error');
 });
-*/
+
 module.exports = app;
